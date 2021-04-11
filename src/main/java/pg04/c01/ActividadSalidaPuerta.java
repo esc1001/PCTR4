611f76492ab2bd0317101f71c4bdf88347d21abf
esc1001 <esc1001@alu.ubu.es>
2021-04-11 20:16:39 +0200

    Exits implementation

--- src/main/java/pg04/c01/ActividadSalidaPuerta.java
+++ src/main/java/pg04/c01/ActividadSalidaPuerta.java
@@ -7,7 +7,7 @@ import java.util.logging.Logger;
 
 public class ActividadSalidaPuerta implements Runnable {
 
-		private static final int NUMENTRADAS = 20;
+		private static final int NUMSALIDAS = 20;
 		private String puerta;
 		private IParque parque;
 
@@ -18,10 +18,10 @@ public class ActividadSalidaPuerta implements Runnable {
 
 		@Override
 		public void run() {
-			for (int i = 0; i < NUMENTRADAS; i ++) {
+			for (int i = 0; i < NUMSALIDAS; i ++) {
 				try {
-					parque.salirDelParque(puerta);
 					TimeUnit.MILLISECONDS.sleep(new Random().nextInt(5)*1000);
+					parque.salirDelParque(puerta);
 				} catch (InterruptedException e) {
 					Logger.getGlobal().log(Level.INFO, "Salida interrumpida");
 					Logger.getGlobal().log(Level.INFO, e.toString());
--- src/main/java/pg04/c01/Parque.java
+++ src/main/java/pg04/c01/Parque.java
@@ -5,28 +5,23 @@ import java.util.Hashtable;
 
 public class Parque implements IParque{
 
-	// TODO 
 	private int contadorPersonasTotales;
 	private Hashtable<String, Integer> contadoresPersonasPuerta;
+	private Hashtable<String, Integer> contadoresPersonasPuertaSalida;
 	
-	
-	public Parque() {	// TODO
+	public Parque() {
 		contadorPersonasTotales = 0;
 		contadoresPersonasPuerta = new Hashtable<String, Integer>();
-		// TODO
+		contadoresPersonasPuertaSalida = new Hashtable<String, Integer>();
 	}
 
-
 	@Override
-	public void entrarAlParque(String puerta){		// TODO
+	public void entrarAlParque(String puerta){
 		
 		// Si no hay entradas por esa puerta, inicializamos
 		if (contadoresPersonasPuerta.get(puerta) == null){
 			contadoresPersonasPuerta.put(puerta, 0);
 		}
-		
-		// TODO
-				
 		
 		// Aumentamos el contador total y el individual
 		contadorPersonasTotales++;		
@@ -35,27 +30,32 @@ public class Parque implements IParque{
 		// Imprimimos el estado del parque
 		imprimirInfo(puerta, "Entrada");
 		
-		// TODO
-		
-		
-		// TODO
-		
 	}
 	
 	@Override
 	public void salirDelParque(String puerta) {
-	// 
-	// TODO Método salirDelParque
-	//
+		
+		// Si no hay salidas por esa puerta, inicializamos
+		if (contadoresPersonasPuertaSalida.get(puerta) == null){
+			contadoresPersonasPuertaSalida.put(puerta, 0);
+		}
+		
+		// Aumentamos el contador total y el individual
+		contadorPersonasTotales--;		
+		contadoresPersonasPuertaSalida.put(puerta, contadoresPersonasPuertaSalida.get(puerta)+1);
+		
+		// Imprimimos el estado del parque
+		imprimirInfo(puerta, "Salida");
+		
 	}
 	
 	private void imprimirInfo (String puerta, String movimiento){
 		System.out.println(movimiento + " por puerta " + puerta);
-		System.out.println("--> Personas en el parque " + contadorPersonasTotales); //+ " tiempo medio de estancia: "  + tmedio);
+		System.out.println("--> Personas en el parque " + contadorPersonasTotales);
 		
 		// Iteramos por todas las puertas e imprimimos sus entradas
 		for(String p: contadoresPersonasPuerta.keySet()){
-			System.out.println("----> Por puerta " + p + " " + contadoresPersonasPuerta.get(p));
+			System.out.println("----> Por puerta " + p + " IN[" + contadoresPersonasPuerta.get(p) + "] OUT[" + contadoresPersonasPuerta.get(p) + "]");
 		}
 		System.out.println(" ");
 	}
@@ -69,10 +69,18 @@ public class Parque implements IParque{
 		return sumaContadoresPuerta;
 	}
 	
+	private int sumarContadoresPuertaSalida() {
+		int sumaContadoresPuerta = 0;
+			Enumeration<Integer> iterPuertas = contadoresPersonasPuertaSalida.elements();
+			while (iterPuertas.hasMoreElements()) {
+				sumaContadoresPuerta += iterPuertas.nextElement();
+			}
+		return sumaContadoresPuerta;
+	}
+	
 	protected void checkInvariante() {
-		assert sumarContadoresPuerta() == contadorPersonasTotales : "INV: La suma de contadores de las puertas debe ser igual al valor del contador del parte";
-		// TODO 
-		// TODO
+		assert sumarContadoresPuerta() == contadorPersonasTotales : "INV: La suma de contadores de las puertas de entrada debe ser igual al valor del contador del parte";
+		assert sumarContadoresPuertaSalida() == contadorPersonasTotales : "INV: La suma de contadores de las puertas de salida debe ser igual al valor del contador del parte";
 	}
 
 	protected void comprobarAntesDeEntrar(){	// TODO
